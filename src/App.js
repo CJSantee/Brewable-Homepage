24e43d8d524c3bdaa4411cdce0dffc5f40ac6dde
CJSantee <santeedev@gmail.com>
2022-01-06 17:14:09 -0600

    Added Signup and Login Forms

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import {
 
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/privacy" element={<Privacy/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
   );
 }
